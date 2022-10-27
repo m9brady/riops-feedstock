20431b2048a861ebb1dbeec12df9181b04bd39df
Mike Brady <mike.brady@ec.gc.ca>
2022-10-27 15:55:34 -0400

    move globals inside functions

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -1,26 +1,25 @@
 from datetime import datetime, date, timedelta
+from os.path import basename
 from pangeo_forge_recipes.patterns import ConcatDim, MergeDim, FilePattern
 from pangeo_forge_recipes.recipes import XarrayZarrRecipe
 
 # RIOPS is run every 6 hours (at 00, 06, 12, and 18 UTC).
 # but, MSC usually only has 2-days-worth of data on the datamart.
 # Thus, we use yesterday's date to ensure the feedstock can run
-START_DATE = datetime.combine(date.today() - timedelta(days=1), datetime.min.time())
-YYYYMMDD = START_DATE.strftime("%Y%m%d")
-RIOPS_PS_BASE_URL = (
-    "https://dd.weather.gc.ca/model_riops/netcdf/forecast/polar_stereographic"
-)
-
-
 def make_url(variable, time):
     """Currently hardcoded for 2D variables run at 00Z"""
+    riops_ps_base_url = (
+        "https://dd.weather.gc.ca/model_riops/netcdf/forecast/polar_stereographic"
+    )
+    start_date = datetime.combine(date.today() - timedelta(days=1), datetime.min.time())
+    yyyymmdd = start_date.strftime("%Y%m%d")
     fh = f"{time:03.0f}"
     # vertical level (SFC or DBS-0.5m for 2D, DBS-all for 3D)
     if variable in ["VOTEMPER", "VOSALINE", "VOZOCRTX", "VOMECRTY"]:
         level = "DBS-0.5m"  # Depth-Below-Surface 0.5m
     else:
         level = "SFC"  # Surface
-    url = f"{RIOPS_PS_BASE_URL}/2d/00/{fh}/{YYYYMMDD}T00Z_MSC_RIOPS_{variable}_{level}_PS5km_P{fh}.nc"
+    url = f"{riops_ps_base_url}/2d/00/{fh}/{yyyymmdd}T00Z_MSC_RIOPS_{variable}_{level}_PS5km_P{fh}.nc"
     return url
 
 
@@ -55,9 +54,11 @@ pattern = FilePattern(make_url, variable_merge_dim, time_concat_dim)
 
 
 def process_input(ds, filename):
+    # use the first 8 chars of filename to get the model run date
+    model_run_date = datetime.strptime(basename(filename)[:8], "%Y%m%d")
     ds = ds.drop("polar_stereographic")
     # use an encoding that is valid of hourly data
-    units = f'hours since {START_DATE.strftime("%Y-%m-%d")} 00:00:00'
+    units = f'hours since {model_run_date.strftime("%Y-%m-%d")} 00:00:00'
     ds.time.encoding = {"units": units, "calendar": "proleptic_gregorian"}
     return ds
 
